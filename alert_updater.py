327bcb1a4e48aab3c6a9b156a1d2170147c7559c
Ryan Harris <rharris@seleniccoding.com>
2021-01-12 09:43:14 -0500

    Initial Commit

---
+++ alert_updater.py
@@ -0,0 +1,12 @@
+
+from models.alert import Alert
+
+alerts = Alert.all()
+
+for alert in alerts:
+    alert.load_item_price()
+    alert.notify_if_price_reached()
+
+if not alerts:
+    print("No alerts have been created. Add item and an alert to begin")
+
